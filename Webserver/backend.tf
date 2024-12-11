c9d66d77679345d71c25bd0894ed8ee839e10dff
Hamza Siddiqui <93541645+HamzaSid020@users.noreply.github.com>
2024-12-11 17:44:28 -0500

    Update backend.tf

--- Webserver/backend.tf
+++ Webserver/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws-bucket-dhana1"         
+    bucket = "aws-bucket-project1"         
     key    = "webserver/terraform.tfstate"  
     region = "us-east-1"              
   }
