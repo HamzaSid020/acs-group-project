9c16d248f2393c180187bb3cc3443cb53625b8f2
Hamza Siddiqui <93541645+HamzaSid020@users.noreply.github.com>
2024-12-11 17:43:54 -0500

    Update config.tf

--- Network/config.tf
+++ Network/config.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "aws-bucket-project" // Bucket from where to GET Terraform State
+    bucket = "aws-bucket-project1" // Bucket from where to GET Terraform State
     key    = "network/terraform.tfstate"
     region = "us-east-1" // Region where bucket created
   }
