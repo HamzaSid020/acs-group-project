f05173cacc0f5e3bd67e9cc4be380fc922dfb98c
Hamza Siddiqui <93541645+HamzaSid020@users.noreply.github.com>
2024-12-11 17:31:50 -0500

    Update config.tf

--- Network/config.tf
+++ Network/config.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "aws-bucket-dhana1" // Bucket from where to GET Terraform State
+    bucket = "arpit-dhana-project" // Bucket from where to GET Terraform State
     key    = "network/terraform.tfstate"
     region = "us-east-1" // Region where bucket created
   }
